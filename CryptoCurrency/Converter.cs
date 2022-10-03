9750da913cb27b9c542c067cc5903d7de004a8fe
victoria-klujeff <61143804+victoria-klujeff@users.noreply.github.com>
2022-10-03 16:11:15 +0200

    Initial commit

---
+++ CryptoCurrency/Converter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CryptoCurrency
+{
+    public class Converter
+    {
+        /// <summary>
+        /// Angiver prisen for en enhed af en kryptovaluta. Prisen angives i dollars.
+        /// Hvis der tidligere er angivet en værdi for samme kryptovaluta, 
+        /// bliver den gamle værdi overskrevet af den nye værdi
+        /// </summary>
+        /// <param name="currencyName">Navnet på den kryptovaluta der angives</param>
+        /// <param name="price">Prisen på en enhed af valutaen målt i dollars. Prisen kan ikke være negativ</param>
+        public void SetPricePerUnit(String currencyName, double price)
+        {
+
+        }
+
+        /// <summary>
+        /// Konverterer fra en kryptovaluta til en anden. 
+        /// Hvis en af de angivne valutaer ikke findes, kaster funktionen en ArgumentException
+        /// 
+        /// </summary>
+        /// <param name="fromCurrencyName">Navnet på den valuta, der konverterers fra</param>
+        /// <param name="toCurrencyName">Navnet på den valuta, der konverteres til</param>
+        /// <param name="amount">Beløbet angivet i valutaen angivet i fromCurrencyName</param>
+        /// <returns>Værdien af beløbet i toCurrencyName</returns>
+        public double Convert(String fromCurrencyName, String toCurrencyName, double amount) 
+        {
+            return 0;
+        }
+    }
+}
